c810216368b74f892a5bbf3eb7cc5b8ab7cea7e0
elias <eliassilva0045@gmail.com>
2024-07-03 21:18:51 -0300

    Initial commit

---
+++ src/main/java/com/cashable/bankify/BankifyApplication.java
@@ -0,0 +1,13 @@
+package com.cashable.bankify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cashable/bankify/BankifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashable.bankify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
