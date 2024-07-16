5e09d6b4b7a59e8bb47597b92c53a0d38b12008b
elias <eliassilva0045@gmail.com>
2024-07-15 22:28:13 -0300

    Hotfix: enable CORS in the API

--- src/main/java/com/cashable/bankify/BankifyApplication.java
+++ src/main/java/com/cashable/bankify/BankifyApplication.java
@@ -1,5 +1,6 @@
 package com.cashable.bankify;
 
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Bankify", version = "1", description = "API RESTful da aplicação Bankify"))
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")},
+		info = @Info(title = "Bankify", version = "1", description = "API RESTful da aplicação Bankify")
+		)
 public class BankifyApplication {
 
 	public static void main(String[] args) {
